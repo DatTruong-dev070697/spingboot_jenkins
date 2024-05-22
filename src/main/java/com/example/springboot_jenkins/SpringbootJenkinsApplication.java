6d1ac87f17af2437987d49264dade7b9e0575410
DatTruong-dev070697 <truongnhatdat5.ndt@gmail.com>
2024-05-22 23:33:57 +0700

    dsfsfs

--- src/main/java/com/example/springboot_jenkins/SpringbootJenkinsApplication.java
+++ src/main/java/com/example/springboot_jenkins/SpringbootJenkinsApplication.java
@@ -2,12 +2,13 @@ package com.example.springboot_jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SpringbootJenkinsApplication {
-
+	@GetMapping("/message")
 	public String getMessage(){
 		return "Hello jenkins";
 	}
